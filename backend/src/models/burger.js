89385ba3b53f90d9911358be93334310ab9203a4
Ian da Silva Correia <54759457+IanDesc@users.noreply.github.com>
2023-06-28 01:44:44 -0300

    Connected to MongoDB and updated models

---
+++ backend/src/models/burger.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const burguerSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    weight: Number,
+    vegan: boolean,
+    price: Number
+});
+
+module.exports = {
+    listBurguer: async function() {
+        const burguerList = await burguerModel.find({}).lean();
+        return burguerList;
+    },
+    
+    insertBurguer: async function(name, description, weight, vegan, price) {
+        const burguer = new burguerModel({
+            name: name,
+            description: description,
+            weight: weight,
+            vegan: vegan,
+            price: price
+        });
+        await burguer.save();
+        return burguer;
+    },
+
+    updateBurguer: async function(id, obj) {
+        let burguer = await burguerModel.findById(id)
+        if (!burguer) {
+            return false
+        }
+        
+        Object.keys(obj).forEach(key => burguer[key] = obj[key])
+        await burguer.save()
+        return burguer
+    },
+
+    deleteBurguer: async function(id) {
+        return await burguerModel.findByIdAndDelete(id)
+    },
+    // getById: async function(id) {
+    //     return await burguerModel.findById(id).lean()
+    // }
+}
